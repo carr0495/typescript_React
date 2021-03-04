443c1649cc762c9bb4127223d46627e23c88ed90
carr0495@algonquinlive.com <carr0495@algonquinlive.com>
2021-03-04 17:37:20 -0500

    created TS react app, recap on datatypes, and component mounting

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Message from "./Message";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component<any> {
+  //#region first lesson
+  // let firstValue: string = "Aiden";
+  // let firstValue: number[] = [2, 4, 5, 6, 9];
+  // let firstValue: boolean = true;
+
+  // //tuple
+  // let aTuple: [string, number] = ["Aiden", 24];
+  // //enumsm
+  // enum Codes {
+  //   first = 1,
+  //   second,
+  // }
+  // //any (dont use it)
+  // let firstName: any = "aiden"; //only for prototyping
+  // //void
+  // const warning = (): void => {
+  //   console.log("Warning");
+  // };
+  //#endregion
+
+  componentWillMount() {
+    //runs first
+    //like constructor
+  }
+
+  componentDidMount() {
+    //runs second
+    //like deconstructor?
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Message message="This is a simple passed message" />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
